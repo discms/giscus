403043af2fea4778507e7a97398a3d896f0b14a0
Nicholas Berlette <nick@berlette.com>
2022-04-21 06:07:43 -0700

    ⬆️  upgrade tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './styles/*.css'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/styles/*.css'
+  ],
   darkMode: 'class',
   theme: {
     extend: {
